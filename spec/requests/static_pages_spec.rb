594d269aeb46d78ce4f017240e21019331750a72
Olya Yakimenko <19.03Michelle@gmail.com>
2013-12-19 13:34:29 +0200

    Contact static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
+    
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
@@ -40,6 +43,20 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('title',
                         :text => " | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => " | Contact")
     end
   end
 end
